fd450881c57d462659b8a7634215c59004e172d0
Athervi Singh <singhathervi001@gmail.com>
2026-05-08 23:32:18 +0530

    sahi

--- pages/_app.js
+++ pages/_app.js
@@ -8,9 +8,6 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>MindVeda - Healing & Growth</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,700;0,800;1,700&family=Great+Vibes&family=Lato:wght@300;400;600&display=swap" rel="stylesheet" />
       </Head>
       <AuthProvider>
         <Component {...pageProps} />
